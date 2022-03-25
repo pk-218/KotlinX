da08c392824879a82a9e0ccc263626fcb930b204
Azeez <officialazeezamd@gmail.com>
2022-03-25 21:33:52 +0530

    chat view model added

--- app/src/main/java/tech/kotlinx/knox/ui/viewmodels/ChatViewModel.kt
+++ app/src/main/java/tech/kotlinx/knox/ui/viewmodels/ChatViewModel.kt
@@ -1,4 +1,38 @@
 package tech.kotlinx.knox.ui.viewmodels
 
-class ChatViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
+import java.util.*
+
+class ChatViewModel : ViewModel() {
+    val TAG = "Chat View Model"
+    private suspend fun sendMessage(msg : String?, receiverIpAddress : String?, receiverPort : Int) {
+        try {
+            val clientSocket : Socket = Socket(receiverIpAddress, receiverPort)
+            val outToServer = clientSocket.getOutputStream()
+            val output = PrintWriter(outToServer)
+            output.println(msg)
+            output.flush()
+            clientSocket.close()
+
+        } catch(e : Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private suspend fun receiveMessage(vararg sockets: Socket): String? {
+        var text : String? = null
+        try {
+            val input = BufferedReader(InputStreamReader(sockets[0].getInputStream()))
+            text = input.readLine()
+            Log.i(TAG, "Received => $text")
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+        return text
+    }
 }
