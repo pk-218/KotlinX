1605815b11b61c3bc25e6cb147b198128838d69b
Prasad <thakareprasad28@yahoo.in>
2022-03-25 21:33:44 +0530

    feat: added recyclerview for chatFragment

---
+++ app/src/main/java/tech/kotlinx/knox/ChatFragment.kt
@@ -0,0 +1,26 @@
+package tech.kotlinx.knox
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import tech.kotlinx.knox.adapter.MessageAdapter
+import tech.kotlinx.knox.data.model.Datasource
+
+class ChatFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_chat, container, false)
+        val messages = Datasource().loadMessages()
+        val recyclerView=view.findViewById<RecyclerView>(R.id.message_view)
+        recyclerView.adapter= context?.let { MessageAdapter(it, messages) }
+
+        return view
+    }
+}
